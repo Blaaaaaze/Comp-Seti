a2759af4401b9eb8ce9ec37e663e096fe6557edf
Igor <112973458+Blaaaaaze@users.noreply.github.com>
2024-06-24 18:31:03 +0300

    Add files via upload

---
+++ lab1/SCRlabNew.bat
@@ -0,0 +1,27 @@
+@echo off
+chcp 65001
+set /p NAME="Введите имя сети "
+set /p settings="выберите настройки (1 - Автоматически (DHCP), 2 - ручной):"
+echo Выбраны настройки - %settings%
+
+if %settings% equ 2 goto manual
+
+:auto
+netsh interface ipv4 set addres %NAME% source=dhcp
+goto end
+
+:manual
+
+set /p IP="Введите ip-адрес: "
+set /p MASK="Введите маску сети:"
+set /p GATEWAY="Введите gateway: "
+
+set /p DNS1="Введите DNS-сервер: "
+
+
+netsh interface ipv4 set address %NAME% static %IP% %MASK% %GATEWAY% 
+netsh interface ipv4 set dnsservers %NAME% static %DNS1% primary
+
+:end
+set /p a="Нажмите Enter для продолжения"
+pause
---
+++ lab1/ScrLab1.ps1
@@ -0,0 +1,39 @@
+﻿param ($name, $type, $IP, $MASK, $GATEWAY, $DNS)
+
+if ($type -eq 1)
+{
+    $IPType = "IPv4"
+    $adapter = Get-NetAdapter | Select-Object $name
+    $interface = $adapter 
+    If ($interface.Dhcp -eq "Disabled") {
+        If (($interface | Get-NetIPConfiguration).Ipv4DefaultGateway) {
+            $interface | Remove-NetRoute -Confirm:$false
+        }
+    $interface | Set-NetIPInterface -DHCP Enabled
+    $interface | Set-DnsClientServerAddress -ResetServerAddresses
+    }
+}
+elseif ($type -eq 2)
+{
+    Set-NetIPInterface -InterfaceAlias $name -Dhcp Disabled
+    $ip_params = @{
+        InterfaceAlias=$name
+        IP=$IP
+        PrefixLength=$MASK
+        DefaultGateway=$GATEWAY
+        
+        AddressFamily = "IPv4"
+    }
+    New-NetIPAddress @ip_params
+
+    $dnsParams = @{
+InterfaceAlias = $name
+ServerAddresses = $DNS
+}
+Set-DnsClientServerAddress @dnsParams
+
+}
+else 
+{
+     Get-NetAdapter $name | Format-List -Property ifDesc, PhysicalMediaType, Speed, FullDuplex 
+} 
